9af28dd2830eee14685d1059d79e5e64387441fa
Akshatjain23 <57037424+Akshatjain23@users.noreply.github.com>
2020-02-27 23:28:41 +0530

    Create add coursehbs.js

---
+++ add coursehbs.js
@@ -0,0 +1,8 @@
+<form action="/course/add" method="post">
+<p>
+ <label> course name <input value"" name="coursename"/>
+   <label> course id<input value"" name="courseid"/>
+  <label> course fee <input value"" name="coursefee"/>
+  <button> add course</button>
+  </p>
+  </form>
